1bb20955e6f56b590b84ad47d94debbd6d258b37
DragonBit <iv.arsenii2@gmail.com>
2024-07-13 21:11:56 +0300

    No gravity?
    No gravity?

---
+++ src/main/java/com/corruptedbyte/whenexperiencefly/WhenExperienceFly.java
@@ -0,0 +1,10 @@
+package com.corruptedbyte.whenexperiencefly;
+
+import net.fabricmc.api.ModInitializer;
+
+public class WhenExperienceFly implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/com/corruptedbyte/whenexperiencefly/client/WhenExperienceFlyClient.java
@@ -0,0 +1,10 @@
+package com.corruptedbyte.whenexperiencefly.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class WhenExperienceFlyClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
